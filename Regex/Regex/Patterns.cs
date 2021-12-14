2aedefaa76a3a54c1a5bb855c549e6f41426cbe6
Rajangane <pratiksharajangane7@gmail.com>
2021-12-14 13:45:44 +0530

    [Pratiksha] ADD : Checking First Name

---
+++ Regex/Regex/Patterns.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPatternMatching
+{
+    public class Patterns
+    {
+        public String REGEX_FIRSTNAME = @"^[A-Z]{3}$"; //First name Contains capital 3 letters
+
+        public bool ValidateFirstName(string name)
+        {
+            
+            return Regex.IsMatch(name, REGEX_FIRSTNAME);
+        }
+    }
+}
--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("*************** Welcome To Regex **********************");
+//Console.WriteLine("*************** Welcome To Regex **********************");
+using RegexPatternMatching;
+Console.Write("Select Number:\n1)FirstName");
+int option = Convert.ToInt32(Console.ReadLine());
+Patterns pattern = new Patterns();
+
+switch (option)
+{
+    case 1:
+        Console.Write("Enter FirstName: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("FirstName:" + pattern.ValidateFirstName(firstName));
+        break;
+
+    default:
+        Console.WriteLine("Invalid Option");
+        break;
+}
