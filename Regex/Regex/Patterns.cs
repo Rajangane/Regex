3fbfd1f5894d11c0b25f2474a59c2e922b73d13c
Rajangane <pratiksharajangane7@gmail.com>
2021-12-14 13:55:05 +0530

    [Pratiksha] ADD : Checking Last Name

--- Regex/Regex/Patterns.cs
+++ Regex/Regex/Patterns.cs
@@ -9,11 +9,17 @@ namespace RegexPatternMatching
 {
     public class Patterns
     {
-        public String REGEX_FIRSTNAME = @"^[A-Z]{3}$"; //First name Contains capital 3 letters
+        public String REGEX_FIRSTNAME = @"^[A-Z]{3}$";//First name Contains capital 3 letters
+        public String REGEX_LASTNAME = @"^[A-Z]{3}$"; //last name contains capital 3 letters
 
         public bool ValidateFirstName(string name)
         {
-            
+
+            return Regex.IsMatch(name, REGEX_FIRSTNAME);
+        }
+        public bool ValidateLastName(string name)
+        {
+
             return Regex.IsMatch(name, REGEX_FIRSTNAME);
         }
     }
--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("*************** Welcome To Regex **********************");
 using RegexPatternMatching;
-Console.Write("Select Number:\n1)FirstName");
+Console.Write("Select Number:\n1)FirstName\n2)LastName");
 int option = Convert.ToInt32(Console.ReadLine());
 Patterns pattern = new Patterns();
 
@@ -12,6 +12,12 @@ switch (option)
         string firstName = Console.ReadLine();
         Console.WriteLine("FirstName:" + pattern.ValidateFirstName(firstName));
         break;
+    case 2:
+        Console.Write("Enter LastName: ");
+        string lasttName = Console.ReadLine();
+        Console.WriteLine("FirstName:" + pattern.ValidateLastName(lasttName));
+        break;
+
 
     default:
         Console.WriteLine("Invalid Option");
